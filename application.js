ad8b0fd3265b3348e7f0c0c175c7032ef3ae802e
andreanat <andrea.nmb13@gmail.com>
2023-10-24 15:51:41 -0600

    aa

--- application.js
+++ application.js
@@ -1,7 +1,8 @@
 /* Global Variables */
+/* Global Variables */
 const apiUrl = 'https://shein-xi-yin-data-service.p.rapidapi.com/product/get_best_sellers_list?country=US&language=en&currency=USD&page=1&size=20';
-const templesElement = document.getElementById('temples');
-const templeList = [];
+const clothesElement = document.getElementById('clothes');
+const clothesList = [];
 
 /* Function to Fetch Data from Shein API */
 async function fetchData() {
@@ -10,7 +11,7 @@ async function fetchData() {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': '21b97010d1msh813e1f0277b243ep120b26jsn616f4ee34816',
-                'X-RapidAPI-Host': 'shein-Xi-Yin-data-service.p.rapidapi.com'
+                'X-RapidAPI-Host': 'shein-xi-yin-data-service.p.rapidapi.com'
             }
         });
         if (response.ok) {
@@ -28,50 +29,28 @@ async function fetchData() {
 /* Function to Display a Random Product */
 function displayRandomProduct() {
     reset();
-    if (templeList.length > 0) {
-        const randomIndex = Math.floor(Math.random() * templeList.length);
-        const randomTemple = templeList[randomIndex];
+    if (clothesList.length > 0) {
+        const randomIndex = Math.floor(Math.random() * clothesList.length);
+        const randomCloth = clothesList[randomIndex];
         
         const articleElement = document.createElement('article');
         
         const h3Element = document.createElement('h3');
-        h3Element.textContent = randomTemple.templeName;
+        h3Element.textContent = randomCloth.productName;
 
         const imgElement = document.createElement('img');
-        imgElement.src = randomTemple.imageUrl;
-        imgElement.alt = randomTemple.location;
+        imgElement.src = randomCloth.imageUrl;
+        imgElement.alt = randomCloth.productName;
         
-        articleElement.appendChild(h3Element);
-        articleElement.appendChild(imgElement);
-        templesElement.appendChild(articleElement);
-    } else {
-        console.log('No temples to display.');
-    }
-}
-
-/* Event Listener for the "Show Random Product" button */
-document.getElementById('showRandomProductButton').addEventListener('click', displayRandomProduct);
-
-
-
-/* Function to Display Clothes */
-function displayClothes(clothesList) {
-    reset();
-    clothesList.forEach((clothe) => {
-        const articleElement = document.createElement('article');
-
-        const h3Element = document.createElement('h3');
-        h3Element.textContent = clothe.productName; 
-
-        const imgElement = document.createElement('img');
-        imgElement.src = clothe.imageUrl; 
-        imgElement.alt = clothe.productName; 
-
         articleElement.appendChild(h3Element);
         articleElement.appendChild(imgElement);
         clothesElement.appendChild(articleElement);
-    });
+    } else {
+        console.log('No products to display.');
+    }
 }
+
+document.getElementById('showRandomProductButton').addEventListener('click', displayRandomProduct);
 
 /* Function to Reset Clothes Element */
 function reset() {
@@ -80,36 +59,11 @@ function reset() {
     }
 }
 
-/* Event Listener */
-document.querySelector('#sortBy').addEventListener('change', () => {
-    const filter = document.getElementById('sortBy').value;
-
-    switch (filter) {
-        case 'sandals':
-            const sandals = clothesList.filter((clothe) => clothe.category === 'sandals'); 
-            displayClothes(sandals);
-            break;
-
-        case 'shirts':
-            const shirts = clothesList.filter((clothe) => clothe.category === 'shirts'); 
-            displayClothes(shirts);
-            break;
-
-        case 'bottom':
-            const bottoms = clothesList.filter((clothe) => clothe.category === 'bottoms');
-            break;
-
-        case 'all':
-        default:
-            displayClothes(clothesList);
-            break;
-    }
-});
-
+/* Main Function */
 async function main() {
     const fetchedData = await fetchData();
-    clothesList.push(...fetchedData.items); 
-    displayTemples(clothesList);
+    clothesList.push(...fetchedData.data); // Make sure the API response structure is correct
+    displayRandomProduct(); // Display a random product when the page loads
 }
 
 main();

f4ade69c3cf18d4919b651145cbe3bd00016ef8d
andreanat <andrea.nmb13@gmail.com>
2023-10-25 16:56:07 -0600

    again

--- application.js
+++ application.js
@@ -1,24 +1,31 @@
-/* Global Variables */
 const url = 'https://shein-xi-yin-data-service.p.rapidapi.com/product/get_best_sellers_list?country=US&language=en&currency=USD&page=1&size=20';
 const clothesElement = document.getElementById('clothes');
-const clothesList = []; 
+const clothesList = [];
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '21b97010d1msh813e1f0277b243ep120b26jsn616f4ee34816',
-		'X-RapidAPI-Host': 'shein-Xi-Yin-data-service.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '21b97010d1msh813e1f0277b243ep120b26jsn616f4ee34816',
+        'X-RapidAPI-Host': 'shein-Xi-Yin-data-service.p.rapidapi.com'
+    }
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+async function fetchData() {
+    try {
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 }
 
-/* function */
+/* function to display random product */
 function displayRandomProduct() {
     if (clothesList.length > 0) {
         const randomIndex = Math.floor(Math.random() * clothesList.length);
@@ -32,7 +39,7 @@ function displayRandomProduct() {
         const imgElement = document.createElement('img');
         imgElement.src = randomCloth.imageUrl;
         imgElement.alt = randomCloth.productName;
-        imgElement.classList.add('product-image'); 
+        imgElement.classList.add('product-image');
         articleElement.appendChild(h3Element);
         articleElement.appendChild(imgElement);
 
@@ -42,33 +49,6 @@ function displayRandomProduct() {
         console.log('No products to display.');
     }
 }
-
-/* function*/
-document.querySelector('#sortBy').addEventListener('change', () => {
-    const filter = document.getElementById('sortBy').value;
-
-    switch (filter) {
-        case 'sandals':
-            const sandals = clothesList.filter((clothe) => clothe.category === 'sandals');
-            displayClothes(sandals);
-            break;
-
-        case 'shirts':
-            const shirts = clothesList.filter((clothe) => clothe.category === 'shirts');
-            displayClothes(shirts);
-            break;
-
-        case 'bottom':
-            const bottoms = clothesList.filter((clothe) => clothe.category === 'bottoms');
-            displayClothes(bottoms);
-            break;
-
-        case 'all':
-        default:
-            displayClothes(clothesList);
-            break;
-    }
-});
 
 /* Main */
 async function main() {
